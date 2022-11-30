919813114afdba5b29baa82cc5c6644ef1963e8d
mattgur11 <75272970+mattgur11@users.noreply.github.com>
2022-11-29 20:25:36 -0500

    Finished up terminal website

--- src/script.js
+++ src/script.js
@@ -1,18 +1,24 @@
 var commands = document.getElementById("commands");
 var user_input = document.getElementById("user_input");
 var terminal_outputs = document.getElementById("terminal_outputs");
+var terminal_content = document.getElementById("terminal_content");
 
 function execute(input){
     let output;
     input = input.toLowerCase();
     output = `<div>→ ${input}</div>`;
     if (!COMMANDS.hasOwnProperty(input)){
-        output += `<div> No such command: ${input}</div>`;
+        output += `<div> <span class="command">No such command: </span>${input}</div>`;
     }
     else{
         output += `<div>${COMMANDS[input]}</div>`;
     }
     terminal_outputs.innerHTML = `${terminal_outputs.innerHTML+output}`;
+    terminal_content.scrollTop = terminal_content.scrollHeight;
+
+    if(input=="github"){
+        setTimeout(() => {window.open("https://github.com/mattgur11", "_blank");}, 2000);
+    }
 }
 
 function key(e){
@@ -32,7 +38,7 @@ document.addEventListener("keypress", key);
 const COMMANDS = 
 {
     help:
-        'Supported commands: ["about", "experience", "education", "skills", "contact"]',
+        'Supported commands: <span class="command-keyword">"about"</span>, <span class="command-keyword">"experience"</span>, <span class="command-keyword">"education"</span>, <span class="command-keyword">"skills"</span>, <span class="command-keyword">"contact"</span>, <span class="command-keyword">"github"</span>',
 
     about:
         "I am a freshman at Penn State studying Computer Science!",
@@ -47,5 +53,8 @@ const COMMANDS =
         "I have coding experience in Python, HTML/CSS/JavaScript, and Java.", 
     
     contact:
-        "Email address: mattgur11@gmail.com"
+        "Email address: mattgur11@gmail.com",
+    
+    github:
+        'Opening my GitHub page...'
 }
